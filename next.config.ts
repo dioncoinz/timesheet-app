57e5d74d247a9106e48fe7904a3119b958a32bff
Dion Georgel <Dion.Georgel@greatland.com.au>
2026-01-29 05:11:52 +0800

    Fix Vercel error and latest UI updates

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
