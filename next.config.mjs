2f1488dce9ac9e904193178b1832d0e3d433ca7b
Dion Georgel <Dion.Georgel@greatland.com.au>
2026-01-29 06:45:48 +0800

    Fix Next config for ESM (next.config.mjs)

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // config options here (leave empty for now)
+};
+
+export default nextConfig;
